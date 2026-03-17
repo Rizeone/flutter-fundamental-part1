6676e8a539c05ca8faa2c7afe30395b219981993
Rizeone <akumagiming@gmail.com>
2026-03-17 14:31:19 +0800

    Add basic widgets (image, dialog, textfield, date_picker, loading_cupertino, FAB, text), assets, images, and updates to main.dart, pubspec.yaml, README.md

---
+++ lib/basic_widgets/textfield_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  const TextFieldWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Contoh TextField")),
+        body: const Padding(
+          padding: EdgeInsets.all(10),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Nama',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
